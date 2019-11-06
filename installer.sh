a76a069b6b0cea71628e26260070197b1f085ee4
rirozizo <rawadzahreddine32@gmail.com>
2019-11-06 14:27:21 +0200

    Added installer.sh
    This will help make things easier for you to install the bot onto any linux system

---
+++ installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+echo "Make sure all files are located in /home/pi/todaymemebot/ (including this installer)"
+echo "Put your own API Key, API Secret, Access Token, and Access Token Secret inside driver.py"
+echo "Creating dummy jpg file so the script works (Don't delete!)"
+touch local_image.jpg
+echo "Modifying crontab to run at noon"
+echo "0 12 * * * /usr/bin/python3 /home/pi/todaymemebot/driver.py > /home/pi/todaymemebot/log" > /var/spool/cron/crontabs/root
+echo "Done! use \"sudo crontab -e\" to edit when you want the script to tweet."
